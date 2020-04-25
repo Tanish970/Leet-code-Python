24ce893cb574bfa73b1d23dc3208b2023951ecff
Tanish970 <rohith.guntur@gmail.com>
2020-04-25 19:02:04 +0530

    Update 201. Bitwise AND of Numbers Range.py

--- 201. Bitwise AND of Numbers Range.py
+++ 201. Bitwise AND of Numbers Range.py
@@ -1,11 +1,12 @@
 class Solution:
     def rangeBitwiseAnd(self, m: int, n: int) -> int:
-        if m==0 or n==0:
-            return 0
-        bot=int(math.log(m,2))
-        top=int(math.log(n,2))
-        if(top!=bot):return 0
-        res=m
-        for i in range(m+1,n+1):
-            res=res&i
-        return res
+        count=0
+        while(m!=n):
+            m=m>>1
+            n=n>>1
+            count+=1
+        return m<<count
+    
+
+#Explaination video link
+https://www.youtube.com/watch?v=-qrpJykY2gE
