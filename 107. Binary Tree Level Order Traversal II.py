114d90fbadeb310b69c6e3495cb45d748979d524
Tanish970 <rohith.guntur@gmail.com>
2020-07-03 18:32:57 +0530

    Create 107. Binary Tree Level Order Traversal II.py

---
+++ 107. Binary Tree Level Order Traversal II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        levels = []
+        if not root:
+            return levels
+        
+        def helper(node,level):
+            
+            if len(levels)==level: 
+                levels.append([])
+            
+            levels[level].append(node.val)
+            
+            if node.left: helper(node.left, level+1)
+            if node.right: helper(node.right, level+1)
+        
+        helper(root,0)
+        return levels[::-1]
